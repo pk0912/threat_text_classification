58fb554de0df1fc3246ecb40695939cf52135b3f
Praveen Kumar <prvnkr.pk@gmail.com>
2020-04-04 17:01:47 +0530

    Added scripts to carry out classification and evaluation process.

--- run.py
+++ run.py
@@ -3,23 +3,37 @@ Entry point for training and predicting
 """
 
 import os
+import numpy as np
 import pandas as pd
-from joblib import dump
+from joblib import dump, load
+from tensorflow.keras.optimizers import Nadam
 
 from utils.helpers import logger, save_csv_data
 from utils.dataset_split import stratified_split
 from raw_data_ingest import get_data
 from preprocessing import preprocess
 from vectorizer import vectorize
+from classification import classifier
 from config import (
     RAW_DATA_INGEST,
     SIMPLE_PROCESSING_TYPE,
     COMPLEX_PROCESSING_TYPE,
     VECTORIZE_DATA_SIMPLE,
     VECTORIZE_DATA_COMPLEX,
-    PERFORM_CLASSIFICATION,
+    SIMPLE_DATA_CLASSIFICATION,
+    COMPLEX_DATA_CLASSIFICATION,
+    GLOVE_PATH,
     MAX_SEQUENCE_LENGTH,
     MAX_VOCAB_SIZE,
+    EMBEDDING_DIM,
+    CLASSES,
+    BATCH_SIZE,
+    EPOCHS,
+    EARLY_STOP_PATIENCE,
+    LOSS,
+    METRICS,
+    MODEL_NAME,
+    MODEL_SAVE_FORMAT,
 )
 from settings import (
     RAW_DATA_DIR,
@@ -28,7 +42,28 @@ from settings import (
     OBJECTS_DIR,
     TRAIN_DATA_DIR_WI,
     VAL_DATA_DIR_WI,
+    OUTPUTS_DIR,
+    VERSION,
 )
+
+
+def get_config():
+    config_dict = dict()
+    config_dict["glove_path"] = GLOVE_PATH
+    config_dict["vocab_size"] = MAX_VOCAB_SIZE
+    config_dict["seq_len"] = MAX_SEQUENCE_LENGTH
+    config_dict["embed_dim"] = EMBEDDING_DIM
+    config_dict["classes"] = CLASSES
+    config_dict["outputs_path"] = OUTPUTS_DIR
+    config_dict["objects_path"] = OBJECTS_DIR
+    config_dict["model_name"] = MODEL_NAME + str(VERSION) + MODEL_SAVE_FORMAT
+    config_dict["loss"] = LOSS
+    config_dict["optimizer"] = Nadam(lr=0.001, beta_1=0.9, beta_2=0.999)
+    config_dict["metrics"] = [METRICS]
+    config_dict["batch_size"] = BATCH_SIZE
+    config_dict["epochs"] = EPOCHS
+    config_dict["early_stop_pat"] = EARLY_STOP_PATIENCE
+    return config_dict
 
 
 def vectorize_data(data_path, processing_type):
@@ -55,9 +90,22 @@ def vectorize_data(data_path, processing_type):
         logger.error("Error in vectorizing data!!!")
 
 
-def perform_classification():
-    # TODO
-    pass
+def perform_classification(data_type="complex"):
+    train_data = pd.read_csv(
+        os.path.join(TRAIN_DATA_DIR_WI, "train_vectors_{}.csv".format(data_type))
+    )
+    val_data = pd.read_csv(
+        os.path.join(VAL_DATA_DIR_WI, "val_vectors_{}.csv".format(data_type))
+    )
+    configs = get_config()
+    X = train_data[[str(i) for i in np.arange(0, MAX_SEQUENCE_LENGTH, 1)]].values
+    y = train_data[CLASSES].values
+    val_X = val_data[[str(i) for i in np.arange(0, MAX_SEQUENCE_LENGTH, 1)]].values
+    val_y = val_data[CLASSES].values
+    tokenizer = load(os.path.join(OBJECTS_DIR, "tokenizer.joblib"))
+    clf_status = classifier(X, y, (val_X, val_y), tokenizer, configs)
+    if not clf_status:
+        logger.error("Error in performing classification!!!")
 
 
 def main():
@@ -104,8 +152,10 @@ def main():
                 os.path.join(COMPLEX_PROCESSED_DATA_DIR, "train_data_complex.csv"),
                 "complex",
             )
-        if PERFORM_CLASSIFICATION:
-            perform_classification()
+        if SIMPLE_DATA_CLASSIFICATION:
+            perform_classification("simple")
+        if COMPLEX_DATA_CLASSIFICATION:
+            perform_classification("complex")
     except Exception as e:
         logger.error("Exception in main method : {}".format(str(e)))
         return
